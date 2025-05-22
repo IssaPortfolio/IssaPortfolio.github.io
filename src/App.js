5e3e640cedd63f02870277bf78c6a7fec8266ea3
IssaPortfolio <Habeeb.H.Issa@gmail.com>
2025-05-21 20:20:02 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Components
 import Navbar from './components/Navbar';
